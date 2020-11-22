7d0c5e49850908eb05d06e3b65258aa611a26e6f
john kuras <w7og@yahoo.com>
2020-11-22 01:25:54 -0800

    removed some obsolete pragmas

--- SetParseFmt.cpp
+++ SetParseFmt.cpp
@@ -9,8 +9,6 @@
 #pragma message( "Compiling " __FILE__ " on " __DATE__ " at " __TIME__ )
 #pragma message( "File last modified on " __TIMESTAMP__ )
 #pragma message( "  ")
-#pragma title( "My Secret Box version 2.0" )
-#pragma subtitle( "Copyright (c) 2003 - 2015, Nehemiah Ministries, Inc." )
 #pragma comment( compiler )
 #pragma comment( user, "File: " __FILE__ ". Compiled on " __DATE__ " at " __TIME__ ".  Last modified on " __TIMESTAMP__ )
 #endif
